4b1f4b3de8b97e9b4e8cc5d025f71d2e05eea0d4
Vaibhav <ishuvaibhav@gmail.com>
2017-09-09 13:28:44 -0400

    implementing nearby

--- PennAppsXVI-App/app/src/main/java/com/example/vaibhav/pennappsxvi_app/MainActivity.java
+++ PennAppsXVI-App/app/src/main/java/com/example/vaibhav/pennappsxvi_app/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,144 +17,139 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.PublishCallback;
+import com.google.android.gms.nearby.messages.PublishOptions;
+import com.google.android.gms.nearby.messages.Strategy;
+import com.google.android.gms.nearby.messages.SubscribeCallback;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private TableLayout table;
     private DatabaseReference mDatabase;
     private FirebaseDatabase database;
     private ArrayList<String> locationsList = new ArrayList<String>();
 
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
+
+    private static final int TTL_IN_SECONDS = 3 * 60; // Three minutes.
+    private static final String KEY_UUID = "key_uuid";
+    private static final Strategy PUB_SUB_STRATEGY = new Strategy.Builder()
+            .setTtlSeconds(TTL_IN_SECONDS).build();
+
+    private Switch mPublishSwitch;
+    private Switch mSubscribeSwitch;
+    private Message mPubMessage;
+    private MessageListener mMessageListener;
+    private ArrayAdapter<String> mNearbyDevicesArrayAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         FirebaseApp.initializeApp(this);
 
         setContentView(R.layout.activity_main);
 
-        database = FirebaseDatabase.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        Log.d("TESTING", "Before Database");
+
         DatabaseReference devicesRef = mDatabase.child("aea925bad7343ffc").child("num_devices");
         DatabaseReference locationRef = mDatabase.child("aea925bad7343ffc").child("location");
         DatabaseReference AT1 = mDatabase.child("aea925bad7343ffc");
-        Log.d("TESTING", "After Database");
+
 
         table = (TableLayout) findViewById(R.id.MainTableLayout);
         String tmp = devicesRef.toString();
-        Log.d("TESTING", "Value of tmp: " + tmp);
-
-        /*
-        devicesRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                Long value = (Long)dataSnapshot.getValue();
-                Log.d("TESTING", "Value is: " + value);
-
-                for(int i = 0; i < table.getChildCount(); i++) {
-                    View view = table.getChildAt(i);
-                    if (view instanceof TableRow) {
-                        // then, you can remove the the row you want...
-                        // for instance...
-                        TableRow row = (TableRow) view;
-                        for(int j = 0; j < row.getChildCount(); j++) {
-
-                            TextView tv = (TextView)row.getChildAt(1);
-                            tv.setText(value.toString());
-
-                        }
-                    }
-                }
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("TESTING", "Failed to read value.", error.toException());
-            }
-        });
-        */
+        Log.d(TAG, "Value of tmp: " + tmp);
 
         AT1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String locValue = (String)dataSnapshot.child("location").getValue();
-                Long numDevicesValue = (Long)dataSnapshot.child("num_devices").getValue();
+                String locValue = (String) dataSnapshot.child("location").getValue();
+                Long numDevicesValue = (Long) dataSnapshot.child("num_devices").getValue();
 
-                Log.d("TESTING", "Location Value is: " + locValue);
-                Log.d("TESTING", "Devices Value is: " + numDevicesValue);
+                Log.d(TAG, "Location Value is: " + locValue);
+                Log.d(TAG, "Devices Value is: " + numDevicesValue);
 
                 boolean locationFound = false;
 
-                for(int i = 1; i < table.getChildCount(); i++) {
-                    View view = table.getChildAt(i);
-                    if (view instanceof TableRow) {
-                        // then, you can remove the the row you want...
-                        // for instance...
-                        TableRow row = (TableRow) view;
-                        for(int j = 0; j < row.getChildCount(); j++) {
-                            TextView tv = (TextView) row.getChildAt(0);
-                            String curText = tv.getText().toString();
-                            if (locValue == curText) {
-                                locationFound = true;
-                                TextView devicesView = (TextView)row.getChildAt(1);
-                                devicesView.setText(numDevicesValue.toString());
-                            }
+
+                View view = table.getChildAt(1);
+                if (view instanceof TableRow) {
+                    // then, you can remove the the row you want...
+                    // for instance...
+                    TableRow row = (TableRow) view;
+                    for (int j = 0; j < row.getChildCount(); j++) {
+                        TextView tv = (TextView) row.getChildAt(0);
+                        String curText = tv.getText().toString();
+                        if (locValue == curText) {
+                            locationFound = true;
+                            TextView devicesView = (TextView) row.getChildAt(1);
+                            devicesView.setText(numDevicesValue.toString());
                         }
-
-                        if (!locationFound) {
-                            int numberOfLocations = locationsList.size();
-                            Log.d("TESTING", "numofLocs Value is: " + numberOfLocations);
-                            TableRow curRow = (TableRow)table.getChildAt(numberOfLocations+1);
-
-                            TextView curLocView = (TextView)curRow.getChildAt(0);
-                            TextView curDevView = (TextView)curRow.getChildAt(1);
-
-                            Log.d("TESTING", "Location Value is: " + curLocView.getText());
-                            Log.d("TESTING", "Devices Value is: " + curDevView.getText());
-
-                            curLocView.setText(locValue);
-                            curDevView.setText(numDevicesValue.toString());
-                        }
-
-
                     }
+
+                    if (!locationFound) {
+                        int numberOfLocations = locationsList.size();
+                        Log.d(TAG, "numofLocs Value is: " + numberOfLocations);
+                        TableRow curRow = (TableRow) table.getChildAt(numberOfLocations + 1);
+
+                        TextView curLocView = (TextView) curRow.getChildAt(0);
+                        TextView curDevView = (TextView) curRow.getChildAt(1);
+
+                        Log.d(TAG, "Location Value is: " + curLocView.getText());
+                        Log.d(TAG, "Devices Value is: " + curDevView.getText());
+
+                        curLocView.setText(locValue);
+                        curDevView.setText(numDevicesValue.toString());
+                    }
+
+
                 }
+
 
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
-                Log.w("TESTING", "Failed to read value.", error.toException());
+                Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
 
     }
-
-    /*
-    public void UpdateTable(String value){
-
-        for(int i = 0; i < table.getChildCount(); i++) {
-            View view = table.getChildAt(i);
-            if (view instanceof TableRow) {
-
-                TableRow row = (TableRow) view;
-
-                for(int j = 0; j < row.getChildCount(); j++) {
-                    TextView tv = (TextView)row.getChildAt(j);
-                    tv.setText(value);
-                }
-
-            }
-        }
-    }
-    */
 
 }
