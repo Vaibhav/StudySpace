06356c510bd51c088157d64456340acefb338415
Vaibhav <ishuvaibhav@gmail.com>
2017-09-09 03:33:06 -0400

    Connected to FireBase

--- PennAppsXVI-App/app/src/main/java/com/example/vaibhav/pennappsxvi_app/MainActivity.java
+++ PennAppsXVI-App/app/src/main/java/com/example/vaibhav/pennappsxvi_app/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.vaibhav.pennappsxvi_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        FirebaseApp.initializeApp(this);
 
+        setContentView(R.layout.activity_main);
+        
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
 
 
 
